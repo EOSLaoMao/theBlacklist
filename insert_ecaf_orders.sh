3e6d15f688a059203fc23a9dbf2e0a5f1dc75347
ZhaoYu <zhaoyu.johnny@gmail.com>
2018-08-14 16:37:45 +0900

    add script to fetch table content as nodeos config

---
+++ insert_ecaf_orders.sh
@@ -0,0 +1,9 @@
+cleos push action theblacklist set '{"accounts": ["blacklistmee","ge2dmmrqgene","gu2timbsguge","ge4tsmzvgege","gezdonzygage","ha4tkobrgqge","ha4tamjtguge","gq4dkmzzhege"], "order_name": "ECAF_Arbitrator_Order_2018-06-19-AO-001", "action": "add"}' -p ecafofficial
+
+cleos push action theblacklist set '{"accounts": ["gu2teobyg4ge","gq4demryhage","q4dfv32fxfkx","ktl2qk5h4bor","haydqnbtgene","g44dsojygyge","guzdonzugmge","ha4doojzgyge","gu4damztgyge","haytanjtgige","exchangegdax","cmod44jlp14k","2fxfvlvkil4e","yxbdknr3hcxt","yqjltendhyjp","pm241porzybu","xkc2gnxfiswe","ic433gs42nky","fueaji11lhzg","w1ewnn4xufob","ugunxsrux2a3","gz3q24tq3r21","u5rlltjtjoeo","k5thoceysinj","ebhck31fnxbi","pvxbvdkces1x","oucjrjjvkrom"], "order_name": "ECAF_Arbitrator_Order_2018-06-22-AO-002", "action": "add"}' -p ecafofficial
+
+cleos push action theblacklist set '{"accounts": ["neverlandwal", "tseol5n52kmo", "potus1111111"], "order_name": "ECAF-Temporary-Freeze-Order-2018-07-13-AO-003", "action": "add"}' -p ecafofficial
+
+cleos push action theblacklist set '{"accounts": ["craigspys211"], "order_name": "ECAF-Order-of-Emergency-Protection-2018-07-19-AO-004-Reissue", "action": "add"}' -p ecafofficial
+
+cleos push action theblacklist set '{"accounts": ["eosfomoplay1"], "order_name": "ECAF-Order-of-Emergency-Protection-2018-08-07-AO-005", "action": "add"}' -p ecafofficial
