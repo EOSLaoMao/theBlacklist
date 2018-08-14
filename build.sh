d171672df2f868d205dac88b2a1a34b94660f1cb
ZhaoYu <zhaoyu.johnny@gmail.com>
2018-08-14 15:32:22 +0900

    rename compile_and_run.sh to build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ NAME=theblacklist
 docker ps | grep $NAME-eos-dev
 if [ $? -ne 0 ]; then
     echo "Run eos dev env "
-    docker run --name $NAME-eos-dev -dit --rm -v  `(pwd)`:/$NAME lemonswang/eos-dev
+    docker run --name $NAME-eos-dev -dit --rm -v  `(pwd)`:/$NAME eoslaomao/eos-dev
 fi
 
 docker exec $NAME-eos-dev eosiocpp -o /$NAME/$NAME.wast /$NAME/$NAME.cpp
