870bc48b05df9aa3bce6f824096da2b56d4efa17
ZhaoYu <zhaoyu.johnny@gmail.com>
2018-09-08 14:11:23 +0900

    make check_blacklish.sh compatible with OS X

--- check_blacklist.sh
+++ check_blacklist.sh
@@ -5,6 +5,14 @@ default_config="./config.ini"
 api=${default_api}
 config=${default_config}
 
+UNAME=`uname`
+
+if [[ $UNAME == 'Darwin' ]]; then
+  checker='shasum -a 256'
+else
+  checker='sha256sum'
+fi
+
 init() {
     [ "$1" != "" ] && api=$1
     [ "$2" != "" ] && config=$2
@@ -20,7 +28,7 @@ get_chain_actor_list() {
 }
 
 get_local_actor_list() {
-    local_actor_list=`cat ${config} | grep actor-black | grep -v "#" |egrep -o '\w+$'| sort`
+    local_actor_list=`cat ${config} | grep actor-black | grep -v "#" |egrep -o '\w+$'| sort | uniq`
 }
 
 check_diff() {
@@ -29,9 +37,9 @@ check_diff() {
 
 # check local and theblacklist actor-blacklist hash
 check_hash() {
-    local_hash=`cat ${config} | grep actor-black | grep -v "#" | sort | tr -d " " | sha256sum`
+    local_hash=`cat ${config} | grep actor-black | grep -v "#" | sort | uniq | tr -d " " | ${checker}`
     # get hash from table theblacklist
-    chain_hash=`echo "${chain_actor_list}" | sed 's/^/actor-blacklist = /g' | tr -d " " | sha256sum`
+    chain_hash=`echo "${chain_actor_list}" | sed 's/^/actor-blacklist = /g' | tr -d " " | ${checker}`
     if [ "${local_hash}" == "${chain_hash}" ];then
         echo "success: ${chain_hash}"
     else
