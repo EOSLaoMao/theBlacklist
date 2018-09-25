6ed8af7db7173ca1ba9453ac52b01a68060ae146
zhi-cike <zhi_cike@163.com>
2018-09-25 18:45:10 +0800

    update check blacklist

--- check_blacklist.sh
+++ check_blacklist.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+default_bp="eoslaomaocom"
 default_api="https://api1.eosasia.one"
 default_config="./config.ini"
 api=${default_api}
 config=${default_config}
+bp=${default_bp}
 
 UNAME=`uname`
 
@@ -13,16 +15,21 @@ else
   checker='sha256sum'
 fi
 
-init() {
-    [ "$1" != "" ] && api=$1
-    [ "$2" != "" ] && config=$2
-    [ ! -f ${config} ] && echo "please check config path(${config})" && exit 1
+_help() {
+    echo ""
+    echo "Usage: $0 [-b BP] [-a API] [-c CONFIG]"
+    echo "          -b bp account"
+    echo "          -a api"
+    echo "          -c config file"
+    echo "Examples:"
+    echo "  $0 -b bp ${default_bp} -a ${default_api} -c ${default_config}"
+    echo ""
 }
 
 get_chain_actor_list() {
     url="${api}/v1/chain/get_table_rows"
-    chain_actor_list=`curl -s ${url} -X POST -d '{"scope":"theblacklist", "code":"theblacklist", "table":"theblacklist", "json": true, "limit": 100
- }' | python  -c "import sys, json; rows = json.load(sys.stdin)['rows'];result = ''; configs = []; [configs.extend([('# %s = %s' % (r['type'], a) if r['action'] != 'add' else '%s = %s' % (r['type'], a)) for a in r['accounts']])for r in rows]; dups = [configs[j] for i in range(len(configs)) for j in range(i+1, len(configs)) if('# ' + configs[i] == configs[j])]; actors=[]; l = [(a if (r['action'] != 'remove' or '# %s = %s' % (r['type'], a) not in dups) else '' for a in r['accounts']) for r in rows]; [actors.extend(list(l)) for l in l]; actors = list(set([a for a in actors if len(a)])); actors.sort(); print '\n'.join(actors)"`
+    chain_actor_list=`curl -s ${url} -X POST -d '{"scope":"theblacklist", "code":"theblacklist", "table":"theblacklist", "json": true, "limit": 100}' | python  -c "import sys, json; rows = json.load(sys.stdin)['rows'];result = ''; configs = []; [configs.extend([('# %s = %s' % (r['type'], a) if r['action'] != 'add' else '%s = %s' % (r['type'], a)) for a in r['accounts']])for r in rows]; dups = [configs[j] for i in range(len(configs)) for j in range(i+1, len(configs)) if('# ' + configs[i] == configs[j])]; actors=[]; l = [(a if (r['action'] != 'remove' or '# %s = %s' % (r['type'], a) not in dups) else '' for a in r['accounts']) for r in rows]; [actors.extend(list(l)) for l in l]; actors = list(set([a for a in actors if len(a)])); actors.sort(); print '\n'.join(actors)"`
+    [ "${chain_actor_list}" == "" ] && echo "please check api(${api})." && exit 1
 }
 
 get_local_actor_list() {
@@ -39,19 +46,48 @@ check_hash() {
     # get hash from table theblacklist
     chain_hash=`echo "${chain_actor_list}" | sed 's/^/actor-blacklist = /g' | tr -d " " | ${checker}`
     if [ "${local_hash}" == "${chain_hash}" ];then
-        echo "success: ${chain_hash}"
+        echo "local success: ${chain_hash}"
     else
         echo "local: ${local_hash}"
         echo "chain: ${chain_hash}"
     fi
 }
 
+# check producerhash and theblacklist actor-blacklist hash
+check_producerhash() {
+    producer_hash=`curl -s ${url} POST -d '{"scope":"theblacklist", "code":"theblacklist", "table":"producerhash", "json": true, "limit": 600}' | jq -r --arg BP "${bp}" '.rows[]|select(.producer == $BP)|.hash'`
+    if [ "${producer_hash}  -" == "${chain_hash}" ];then
+        echo "producer_hash success: ${chain_hash}"
+    else
+        echo "producer hash: ${producer_hash}"
+        echo "chain: ${chain_hash}"
+    fi
+}
+
+process_args() {
+    while getopts :hb:a:c: opt
+    do
+        case "${opt}" in
+            h) _help && exit 0 ;;
+            b) bp="${OPTARG}" ;;
+            a) api="${OPTARG}" ;;
+            c) config="${OPTARG}" ;;
+            :) echo "Error: Option \"-${OPTARG}\" requires an argument." && _help && exit 1;;
+            \?) echo "Error: Unknown option: \"-${OPTARG}\"." && _help && exit 1;;
+        esac
+    done
+
+    shift $(( ${OPTIND} - 1 ))
+    [ ! -f ${config} ] && echo "please check config path(${config})" && exit 1
+}
+
 main() {
-    init $@
+    process_args $@
     get_chain_actor_list
     get_local_actor_list
     check_diff
     check_hash
+    check_producerhash
 }
 
 main $@
